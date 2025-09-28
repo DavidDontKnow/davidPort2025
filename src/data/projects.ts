6c6ae2a9913226375eeaa64779eb33e61b56b953
DavidDontKnow <DavidDontKnow02@gmail.com>
2025-09-27 21:18:20 -0700

    2 projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,6 @@
 import { Project } from "../types/projectTypes";
 import projectOne from "../assets/images/APlusQuiz.png";
-import projectFour from "../assets/images/DaveedDev.png";
+// import projectFour from "../assets/images/DaveedDev.png";
 import projectTwo from "../assets/images/prodLaunch.png";
 
 export const PROJECTS: Project[] = [
@@ -23,22 +23,22 @@ export const PROJECTS: Project[] = [
     link: "https://github.com/DavidDontKnow/productivityLauncher",
     repo: "https://github.com/DavidDontKnow/productivityLauncher",
   },
-  {
-    id: 3,
-    title: "Upcoming Project",
-    description: "An upcoming project description.",
-    technologies: ["React", "Node.js", "MongoDB"],
-    image: "https://placehold.co/600x400",
-    link: "https://example.com/project3",
-    repo: "https://github.com",
-  },
-  {
-    id: 4,
-    title: "David's Portfolio",
-    description: "This is a look into this portfolio.",
-    technologies: ["React", "TypeScript", "Tailwind CSS"],
-    image: projectFour,
-    link: "https://daveed.dev",
-    repo: "https://github.com",
-  },
+  // {
+  //   id: 3,
+  //   title: "Upcoming Project",
+  //   description: "An upcoming project description.",
+  //   technologies: ["React", "Node.js", "MongoDB"],
+  //   image: "https://placehold.co/600x400",
+  //   link: "https://example.com/project3",
+  //   repo: "https://github.com",
+  // },
+  // {
+  //   id: 4,
+  //   title: "David's Portfolio",
+  //   description: "This is a look into this portfolio.",
+  //   technologies: ["React", "TypeScript", "Tailwind CSS"],
+  //   image: projectFour,
+  //   link: "https://daveed.dev",
+  //   repo: "https://github.com",
+  // },
 ];
